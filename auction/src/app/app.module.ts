6b6c545e966e732fe114130cd02d0782652be944
Zehua Wang <zehwang@microsoft.com>
2018-03-06 20:42:20 +0800

    finish unit 2 and establish a website

--- auction/src/app/app.module.ts
+++ auction/src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { StockManagerComponent } from './stock/stock-manager/stock-manager.component';
+import { StarsComponent } from './stars/stars.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    MenuComponent,
+    SidebarComponent,
+    StockManagerComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule
