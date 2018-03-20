5dfaaed75b4630254594c999a246e07c914ae052
Zehua Wang <zehwang@microsoft.com>
2018-03-20 12:53:07 +0800

    add component communication

--- auction/src/app/stars/stars.component.ts
+++ auction/src/app/stars/stars.component.ts
@@ -1,22 +1,37 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
 
   @Input()
-  public rating: number = 0;
+  public rating = 0;
+  @Input()
+  public flag = true;
+  @Output()
+  public ratingChange: EventEmitter<number> = new EventEmitter<number>();
 
   public stars: boolean[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(){
+    this.stars = [];
     for (let i = 1; i <= 5; i++) {
       this.stars.push(i > this.rating);
+    }
+  }
+  ngOnInit() {
+  }
+
+  setStar(num: number) {
+    if ( !this.flag) {
+      this.rating = num;
+
+      this.ratingChange.emit(this.rating);
     }
   }
 
